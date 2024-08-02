99024f8ae6379d7a71a2fa2ba9fe3fcae8856368
Nikita Nazarov <nvnazarov.me@gmail.com>
2024-08-02 19:35:58 +0300

    Add two EditText fields to edit game field size

--- app/src/main/java/com/example/snake/MainActivity.kt
+++ app/src/main/java/com/example/snake/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import com.example.snake.game.Game
@@ -20,11 +21,32 @@ class MainActivity : ComponentActivity(), OnClickListener {
         setContentView(R.layout.layout_main)
 
         val gameView = findViewById<GameView>(R.id.game_view)
+        val editWidth = findViewById<EditText>(R.id.edit_width)
+        val editHeight = findViewById<EditText>(R.id.edit_height)
         val startBtn = findViewById<Button>(R.id.btn_start)
 
         startBtn.setOnClickListener {
+            var width: Int
+            var height: Int
+            try {
+                width = editWidth.text.toString().toInt()
+                height = editHeight.text.toString().toInt()
+
+                if (width <= 4 || height <= 4) throw IllegalArgumentException()
+            } catch (e: Exception) {
+                width = 10
+                height = 10
+                editWidth.setText(width.toString())
+                editHeight.setText(height.toString())
+            }
+
             startBtn.setText(R.string.restart)
-            game = Game(gameView, width = 20, height = 20, updatePeriod = 150L)
+            game = Game(
+                gameView,
+                width,
+                height,
+                updatePeriod = 150L
+            )
             game?.start()
         }
     }
